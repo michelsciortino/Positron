32e618cdac5573ec365404c1ee047338582a8444
michelsciortino <michel.sciortino@outlook.com>
2021-12-07 11:49:15 +0100

    Adding Test Layer

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,8 +1,24 @@
 #include <Positron.h>
 
+class TestLayer : public Positron::Layer {
+  public:
+  TestLayer() : Layer("Test") {}
+
+  void OnAttach() override {
+    LOG_INFO("Layer {} attached", m_Name);
+  }
+
+  void OnEvent(Positron::Event& event) override {
+    LOG_INFO("Layer {} received event {}", m_Name, event);
+  }
+};
+
 class Sandbox : public Positron::Application {
   public:
-  Sandbox() {}
+  Sandbox() : Application() {
+    std::shared_ptr layer = std::make_shared<TestLayer>();
+    m_LayerStack.PushFront(layer);
+  }
 
   ~Sandbox() {}
 };
